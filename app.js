d1f319c224a72e06023c101047973a48c90893f5
Nicolas Sponton <escnsponton@gmail.com>
2022-02-04 01:45:52 -0300

    second version, before id check

--- app.js
+++ app.js
@@ -5,13 +5,23 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const lodash = require("lodash");
 const { slice } = require("lodash");
-const mongoose = require(mongoose);
+const mongoose = require("mongoose");
+
+mongoose.connect('mongodb://localhost:27017/blogDB');
+
+const entrySchema = {
+  title: String,
+  text: String,
+  shortText: String,
+  url: String
+}
+
+const Entry = mongoose.model("entry",entrySchema);
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 var entrys = [];
-var shortEntrys = [];
 
 const app = express();
 
@@ -21,7 +31,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/",(req,res)=>{
-  res.render("home.ejs",{homeText:homeStartingContent,entrys:entrys});
+
+  Entry.find({},(err,entrys)=>{
+    res.render("home.ejs",{homeText:homeStartingContent,entrys:entrys});
+  });
 });
 
 app.get("/about",(req,res)=>{
@@ -41,20 +54,28 @@ app.post("/",(req,res)=>{
   var shortText = req.body.text
   if(shortText.length > 100){shortText = req.body.text.slice(0,100) + "..."};
 
-  entrys.push({
+  const entry = new Entry({
     title: req.body.title,
     text: req.body.text,
-    shortText: (shortText),
+    shortText: shortText,
     url: "/posts/" + req.body.title.toLowerCase()
   });
-  res.redirect("/"); 
+
+  entry.save((err)=>{
+    if(!err){
+      res.redirect("/");
+    }
+  });
 });
 
 app.get("/posts/:smt",(req,res)=>{
-  entrys.forEach((item)=>{
-    if(lodash.lowerCase(req.params.smt) === item.title.toLowerCase()){
-      res.render("post.ejs",item);
-    }
+
+  Entry.find({},(err,entrys)=>{
+    entrys.forEach((item)=>{
+      if(lodash.lowerCase(req.params.smt) === item.title.toLowerCase()){
+        res.render("post.ejs",item);
+      }
+    });
   });
 });
 
