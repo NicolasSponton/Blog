1f99c0d2474cac27d174abcb98f27d1f94c8085d
Nicolas Sponton <escnsponton@gmail.com>
2022-02-25 16:35:42 -0300

    Version 1.0

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ const ejs = require("ejs");
 const lodash = require("lodash");
 const { slice } = require("lodash");
 const mongoose = require("mongoose");
+const { handle } = require("express/lib/application");
 
-mongoose.connect('mongodb://localhost:27017/blogDB');
+//Connection to local DB
+mongoose.connect('mongodb://127.0.0.1:27017/blogDB');
 
+//Set DB collections
 const entrySchema = {
   title: String,
   text: String,
@@ -18,10 +21,11 @@ const entrySchema = {
 
 const Entry = mongoose.model("entry",entrySchema);
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const homeStartingContent = 'Welcome to my blog where you can read all current entrys and even create your owns on the "compose" section, all of them will be stored on a mongoDB DataBase. This web app is made using express to get the server up and running, and ejs templates to simplify html interactions. Entys will only show up to 100 characters on the home screen, if you want to read the complete entry on a separate page, you have to click on "Read more". You can delete entrys clicking on the trashcan.';
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-var entrys = [];
+
+//Prepare set up app, bodyParser, ejs and a static folder
 
 const app = express();
 
@@ -30,11 +34,26 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/",(req,res)=>{
 
+/////// HTTP requests ////////
+
+app.get("/",(req,res)=>{
   Entry.find({},(err,entrys)=>{
     res.render("home.ejs",{homeText:homeStartingContent,entrys:entrys});
   });
+});
+
+app.post("/delete",(req,res)=>{
+  
+    Entry.findByIdAndRemove(req.body._id,(err)=>{
+      if(err){
+        console.log(err);
+      }else{
+        console.log("Successfully deleted item");
+        res.redirect("/");
+      }
+    });
+
 });
 
 app.get("/about",(req,res)=>{
@@ -57,8 +76,7 @@ app.post("/",(req,res)=>{
   const entry = new Entry({
     title: req.body.title,
     text: req.body.text,
-    shortText: shortText,
-    url: "/posts/" + req.body.title.toLowerCase()
+    shortText: shortText
   });
 
   entry.save((err)=>{
@@ -69,10 +87,9 @@ app.post("/",(req,res)=>{
 });
 
 app.get("/posts/:smt",(req,res)=>{
-
   Entry.find({},(err,entrys)=>{
     entrys.forEach((item)=>{
-      if(lodash.lowerCase(req.params.smt) === item.title.toLowerCase()){
+      if(req.params.smt == item._id){
         res.render("post.ejs",item);
       }
     });
@@ -80,12 +97,7 @@ app.get("/posts/:smt",(req,res)=>{
 });
 
 
-
-
-
-
-
-
+//Local server
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
